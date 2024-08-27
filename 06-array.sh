c60c58017d296215641e6b3ec5d12bf7f9ea1591
Bhavishya <bhavishyachowdary185@gmail.com>
2024-08-27 09:40:17 -0400

    variables

--- 06-array.sh
+++ 06-array.sh
@@ -3,8 +3,8 @@
 FRUITS=("APPLE" "KIWI" "ORANGE")
 
 
- echo "First fruit is: ${Fruits[0]}"
- echo "Seconf fruit is: ${Fruits[1]}"
- echo "Third fruit is: ${Fruits[3]}"
+ echo "First fruit is: ${FRUITS[0]}"
+ echo "Seconf fruit is: ${FRUITS[1]}"
+ echo "Third fruit is: ${FRUITS[3]}"
 
- echo "First fruit is: ${Fruits[@]}"
+ echo "First fruit is: ${FRUITS[@]}"
