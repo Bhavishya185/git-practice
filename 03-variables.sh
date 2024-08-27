89cd2afdc068d8ee2773a4b2c07485483aca419b
Bhavishya <bhavishyachowdary185@gmail.com>
2024-08-27 09:07:23 -0400

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Bhavishya
+PERSON2=Teja
 
 echo "$PERSON1: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}: hello $PERSON1. I am fine. how r u dng?"
