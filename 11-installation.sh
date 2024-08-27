9c86c47c6b4b9489751ff975a676b9782304b24d
Bhavishya <bhavishyachowdary185@gmail.com>
2024-08-27 16:44:30 -0400

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "User ID is: $USERID"
+#echo "User ID is: $USERID"
+
+if [ $USEID -ne 0 ]
+then
+    echo "Please run the script with root priveleges"
+fi
+
+dnf install git -y
