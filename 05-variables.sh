048780246db01b2e0a857488bfd46ab7abb29003
Bhavishya <bhavishyachowdary185@gmail.com>
2024-08-27 09:26:03 -0400

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,6 +3,12 @@
 
 echo "please enter your username::"
 
-read USERNAME
+read -s USERNAME
 
 echo "Username entered is: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "Username entered is: $PASSWORD"
