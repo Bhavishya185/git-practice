6de4232670c0dd2cb427260ab65336a04baa3be2
Bhavishya <bhavishyachowdary185@gmail.com>
2024-08-29 08:45:59 -0400

    shell

---
+++ 16-set.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e #setting the automatic exit, if we get error, set -ex for doing
+echo "Hello World success"
+echo "Hello World failure"
+echo "Hello World after failure"
---
+++ 18-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+SOURCE_DIR=$2
+DAYS=${3:-14} #if $3 is empty, default is 14 days
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+USAGE(){
+     echo -e "$R USAGE:: $N sh 18-backup.sh <source> <destination> <days(ooptional)>'
+}
+    
+#check the source and destination are provided
+
+if[ $# -lt 2 ] 
+then
+    USAGE
+fi
